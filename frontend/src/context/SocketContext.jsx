89de78f8a6567dd6ee359d9c900fb5c4d96b28cb
yashwanthhs-oct11 <yashwanthhs110@gmail.com>
2024-10-27 20:17:54 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://yhs-chats.onrender.com", {
         query: {
           userId: authUser._id,
         },
